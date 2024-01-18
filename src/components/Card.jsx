bacb8edb1ebaec982ea1fc70b3993195630578a7
Ivan <iamgilyov@gmail.com>
2024-01-17 20:38:56 -0800

    routing implementation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import { formatAmount } from "../client";
+import { Link } from "react-router-dom";
 
 export default function Card({ id, title, price, img, desc, addToCartHandle }) {
   return (
@@ -11,7 +12,9 @@ export default function Card({ id, title, price, img, desc, addToCartHandle }) {
           <strong>{title}</strong>
           <span>{formatAmount(price)}</span>
         </div>
+        <Link to={`/product/${id}`}>
         <p>{desc}</p>
+        </Link>
         <button onClick={()=> addToCartHandle(id)} className="addToCartBtn">Add to Cart</button>
       </div>
     </div>
