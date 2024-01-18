e5cf612d27e7e205e5cbe19b8ed5894de5f4d535
Aiturgan <aiturgan888@gmail.com>
2024-01-18 16:56:33 -0600

    did the cart fucntionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,25 @@
 import CartItem from './CartItem';
+import { useState, useEffect } from 'react';
 
 const Cart = ({ setCart, cart, toggleCart, addToCartHandle, decreaseQuantity }) => {
 	const deleteProduct = (id) => {
 		const filteredItems = cart.filter((el) => el.id !== id);
 		setCart(filteredItems);
 	};
+	const [totalCost, setTotalCost] = useState(0);
 
 	const deleteAllFromCart = () => {
 		setCart([]);
 	};
+
+	useEffect(() => {
+		let cost = 0;
+
+		for (let i = 0; i < cart.length; i++) {
+			cost += cart[i].quantity * cart[i].price;
+		}
+		setTotalCost(cost);
+	}, [cart]);
 
 	return (
 		<>
@@ -27,12 +38,14 @@ const Cart = ({ setCart, cart, toggleCart, addToCartHandle, decreaseQuantity }) 
 						<CartItem deleteProduct={deleteProduct} decreaseQuantity={decreaseQuantity} addToCartHandle={addToCartHandle} {...item} />
 					))
 				) : (
-					<h3>Your Cart is empty</h3>
+					<h3>Your Cart is empty!</h3>
 				)}
-
-				<div clasName="totalPrice">
-					<h1>Total:</h1>
-					<p>$</p>
+				<div>
+					<hr />
+				</div>
+				<div className="totalPrice">
+					<h4>Total Price:</h4>
+					<h4>${totalCost.toFixed(2)}</h4>
 				</div>
 				{cart.length > 0 && (
 					<button className="deleteBtn" onClick={() => deleteAllFromCart()}>
