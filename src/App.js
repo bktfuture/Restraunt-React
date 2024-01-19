75069e4c8dad86676d07731fae2654d0f4d08f8a
Aiturgan <aiturgan888@gmail.com>
2024-01-18 18:25:21 -0600

    fixed last moments bugs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
-import { useEffect, useState } from "react";
-import "./App.css";
-import { getProducts } from "./client";
-import Cards from "./components/Cards";
-import Search from "./components/Search";
-import Menu from "./components/Menu";
-import Cart from "./components/Cart";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import SingleCard from "./components/SingleCard";
-
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getProducts } from './client';
+import Cards from './components/Cards';
+import Search from './components/Search';
+import Menu from './components/Menu';
+import Cart from './components/Cart';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SingleCard from './components/SingleCard';
 
 function App() {
 	const [products, setProducts] = useState([]);
@@ -41,7 +39,7 @@ function App() {
 	};
 
 	const categories = [...new Set(products.map((product) => product.category))];
-  	// Filter products based on selected category
+	// Filter products based on selected category
 	const filteredProducts = () => {
 		if (selectedCategory === 'all') {
 			return products;
@@ -49,31 +47,25 @@ function App() {
 			return products.filter((product) => product.category === selectedCategory);
 		}
 	};
-  
-  return (
-    <BrowserRouter>
-    
 
-    <div className="body">
-      <Link to="/">
-      <h1>Restaurant Menu</h1>
-      </Link>
-      <Search />
-      <Routes>
-      <Route path="/" element={<Cards products={filteredProducts()} addToCartHandle={addToCartHandle}/>} />
-      <Route path="/product/:productId" element={<SingleCard addToCartHandle={addToCartHandle} products={products} />} />
-    </Routes>
-      
-			<Menu toggleCart={toggleCart} categories={categories} selectedCategory={selectedCategory} onSelectCategory={setSelectedCategory} cart={cart} />
-			{isCartOpen && (
-				<Cart cart={cart} toggleCart={toggleCart} setCart={setCart} addToCartHandle={addToCartHandle} decreaseQuantity={decreaseQuantity} />
-			)}
-
-
-      
-    </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="body">
+				<Link to="/">
+					<h1>Restaurant Menu</h1>
+				</Link>
+				<Menu toggleCart={toggleCart} categories={categories} selectedCategory={selectedCategory} onSelectCategory={setSelectedCategory} cart={cart} />
+				{isCartOpen && (
+					<Cart cart={cart} toggleCart={toggleCart} setCart={setCart} addToCartHandle={addToCartHandle} decreaseQuantity={decreaseQuantity} />
+				)}
+				<Search />
+				<Routes>
+					<Route path="/" element={<Cards products={filteredProducts()} addToCartHandle={addToCartHandle} />} />
+					<Route path="/product/:productId" element={<SingleCard addToCartHandle={addToCartHandle} products={products} />} />
+				</Routes>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
